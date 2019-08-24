0d2641228c09e28d6e3e17b065f37f4443b483db
RichardNie <54984573@qq.com>
2019-08-24 14:49:43 +0800

    daily commit

--- neclib/src/main/java/com/nec/lib/android/utils/ImageUtil.java
+++ neclib/src/main/java/com/nec/lib/android/utils/ImageUtil.java
@@ -1,6 +1,9 @@
 package com.nec.lib.android.utils;
 
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.util.TypedValue;
 
 public class ImageUtil {
 
@@ -10,6 +13,15 @@ public class ImageUtil {
         options.inPurgeable = true;
         options.inSampleSize = inSampleSize;
         return options;
+    }
+
+    public static Bitmap getBitmap(Context context, int resId) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        TypedValue value = new TypedValue();
+        context.getResources().openRawResource(resId, value);
+        options.inTargetDensity = value.density;
+        options.inScaled=false;//不缩放
+        return BitmapFactory.decodeResource(context.getResources(), resId, options);
     }
 
 }
