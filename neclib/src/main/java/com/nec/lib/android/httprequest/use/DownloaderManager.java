acf07894fd6a640073f64e7842e9f6b88512ec52
RichardNie <54984573@qq.com>
2019-08-24 18:22:23 +0800

    daily commit

--- neclib/src/main/java/com/nec/lib/android/httprequest/use/DownloaderManager.java
+++ neclib/src/main/java/com/nec/lib/android/httprequest/use/DownloaderManager.java
@@ -184,7 +184,6 @@ public class DownloaderManager {
             //Chanel NIO中的用法，由于RandomAccessFile没有使用缓存策略，直接使用会使得下载速度变慢，亲测缓存下载3.3秒的文件，用普通的RandomAccessFile需要20多秒。
             channelOut = randomAccessFile.getChannel();
             // 内存映射，直接使用RandomAccessFile，是用其seek方法指定下载的起始位置，使用缓存下载，在这里指定下载位置。
-            System.out.println(body.contentLength());
             MappedByteBuffer mappedBuffer = channelOut.map(FileChannel.MapMode.READ_WRITE, startsPoint, body.contentLength());
             byte[] buffer = new byte[1024];
             int len;
